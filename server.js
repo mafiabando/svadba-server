06bcd93a6bd41b261447ac5167f39fe0431c18ca
Trapboi <trapboiatlanta@gmail.com>
2025-05-13 12:43:09 +0300

    server-fix3

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
 });
 
 // Запуск сервера
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`✅ Сервер запущен на порту ${PORT}`);
 });
